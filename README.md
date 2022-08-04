a58aa2a7cee13c8317623b31918cdd2f852f88e3
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-05 04:20:44 +0700

    update to function method

--- README.md
+++ README.md
@@ -1,63 +1,61 @@
 # Crypto
 
 ### Install
-for nodejs
 
 ```
 npm install @ndiing/crypto
 ```
 
-for web
+### CDN
 
 ```
 https://cdn.jsdelivr.net/gh/ndiing/crypto/crypto.js
 ```
 
-
 ### Usage
 
 ```js
-const crypto = require('@ndiing/crypto')
+const crypto = require("@ndiing/crypto");
 var data = "message";
 var key = crypto.randomBytes(16).toString("hex");
 var iv = crypto.randomBytes(8).toString("hex");
 
-var encrypted = Crypto.encrypt(data, { key, iv });
-console.log(encrypted)
-console.log(Crypto.decrypt(encrypted, { key, iv }));
+var encrypted = encrypt(data, { key, iv });
+console.log(encrypted);
+console.log(decrypt(encrypted, { key, iv }));
 
 var { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-modulusLength: 4096,
-publicKeyEncoding: {
-    type: "spki",
-    format: "pem",
-},
-privateKeyEncoding: {
-    type: "pkcs8",
-    format: "pem",
-},
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: "spki",
+        format: "pem",
+    },
+    privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+    },
 });
-var options={privateKey, publicKey}
-var data='message'
+var options = { privateKey, publicKey };
+var data = "message";
 
-var encrypted = Crypto.privateEncrypt(data, options);
+var encrypted = privateEncrypt(data, options);
 console.log(encrypted);
-console.log(Crypto.publicDecrypt(encrypted, options));
-var encrypted = Crypto.publicEncrypt(data, options);
+console.log(publicDecrypt(encrypted, options));
+var encrypted = publicEncrypt(data, options);
 console.log(encrypted);
-console.log(Crypto.privateDecrypt(encrypted, options));
-var signature=Crypto.sign(encrypted, options)
+console.log(privateDecrypt(encrypted, options));
+var signature = sign(encrypted, options);
 console.log(signature);
-console.log(Crypto.verify(encrypted,signature, options));
+console.log(verify(encrypted, signature, options));
 
-var encoded=Crypto.base64Encode('message')
-console.log(encoded)
-console.log(Crypto.base64Decode(encoded))
+var encoded = base64Encode("message");
+console.log(encoded);
+console.log(base64Decode(encoded));
 
-var encoded=Crypto.base64UrlEncode('message')
-console.log(encoded)
-console.log(Crypto.base64UrlDecode(encoded))
+var encoded = base64UrlEncode("message");
+console.log(encoded);
+console.log(base64UrlDecode(encoded));
 
-console.log(Crypto.hash("password"));
-console.log(Crypto.hmac("password", { key: "secret" }));
+console.log(hash("password"));
+console.log(hmac("password", { key: "secret" }));
 ```
--- index.js
+++ index.js
@@ -1,91 +1,101 @@
 const crypto = require("crypto");
 
-class Crypto {
-    static encrypt(data, options = {}) {
-        const { algorithm = "aes256", key = "", iv = "", chunk = data, encoding = "hex" } = options;
-        const cipher = crypto.createCipheriv(algorithm, key, iv);
-        return Buffer.concat([cipher.update(chunk), cipher.final()]).toString(encoding);
-    }
-
-    static decrypt(data, options = {}) {
-        const { algorithm = "aes256", key = "", iv = "", chunk = data, encoding = "hex" } = options;
-        const cipher = crypto.createDecipheriv(algorithm, key, iv);
-        return Buffer.concat([cipher.update(chunk, encoding), cipher.final()]).toString();
-    }
-
-    static privateEncrypt(data, options = {}) {
-        const { privateKey = "", encoding = "hex" } = options;
-        return crypto.privateEncrypt(privateKey, Buffer.from(data)).toString(encoding);
-    }
-
-    static publicDecrypt(data, options = {}) {
-        const { publicKey = "", encoding = "hex" } = options;
-        return crypto.publicDecrypt(publicKey, Buffer.from(data, encoding)).toString();
-    }
-
-    static publicEncrypt(data, options = {}) {
-        const { publicKey = "", encoding = "hex" } = options;
-        return crypto.publicEncrypt(publicKey, Buffer.from(data)).toString(encoding);
-    }
-
-    static privateDecrypt(data, options = {}) {
-        const { privateKey = "", encoding = "hex" } = options;
-        return crypto.privateDecrypt(privateKey, Buffer.from(data, encoding)).toString();
-    }
-
-    static sign(data, options = {}) {
-        const { algorithm = "sha256", encoding = "hex" } = options;
-        const buffer = crypto.createSign(algorithm);
-        buffer.update(data);
-        buffer.end();
-        return buffer.sign(privateKey, encoding);
-    }
-
-    static verify(data, signature, options = {}) {
-        const { algorithm = "sha256", encoding = "hex" } = options;
-        const buffer = crypto.createVerify(algorithm);
-        buffer.update(data);
-        buffer.end();
-        return buffer.verify(privateKey, signature, encoding);
-    }
-
-    static base64Encode(data) {
-        return Buffer.from(data).toString("base64");
-    }
-
-    static base64Decode(data) {
-        return Buffer.from(data, "base64").toString();
-    }
-
-    static base64UrlEncode(data) {
-        return Buffer.from(data).toString("base64url");
-    }
-
-    static base64UrlDecode(data) {
-        return Buffer.from(data, "base64url").toString();
-    }
-
-    static hash(data, options = {}) {
-        const { algorithm = "sha256", encoding = "hex" } = options;
-        return crypto.createHash(algorithm).update(data).digest(encoding);
-    }
-
-    static hmac(data, options = {}) {
-        const { algorithm = "sha256", key = "", encoding = "hex" } = options;
-        return crypto.createHmac(algorithm, key).update(data).digest(encoding);
-    }
+function encrypt(data, options = {}) {
+    const { algorithm = "aes256", key = "", iv = "", chunk = data, encoding = "hex" } = options;
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    return Buffer.concat([cipher.update(chunk), cipher.final()]).toString(encoding);
 }
-// // Crypto
+
+function decrypt(data, options = {}) {
+    const { algorithm = "aes256", key = "", iv = "", chunk = data, encoding = "hex" } = options;
+    const cipher = crypto.createDecipheriv(algorithm, key, iv);
+    return Buffer.concat([cipher.update(chunk, encoding), cipher.final()]).toString();
+}
+
+function privateEncrypt(data, options = {}) {
+    const { privateKey = "", encoding = "hex" } = options;
+    return crypto.privateEncrypt(privateKey, Buffer.from(data)).toString(encoding);
+}
+
+function publicDecrypt(data, options = {}) {
+    const { publicKey = "", encoding = "hex" } = options;
+    return crypto.publicDecrypt(publicKey, Buffer.from(data, encoding)).toString();
+}
+
+function publicEncrypt(data, options = {}) {
+    const { publicKey = "", encoding = "hex" } = options;
+    return crypto.publicEncrypt(publicKey, Buffer.from(data)).toString(encoding);
+}
+
+function privateDecrypt(data, options = {}) {
+    const { privateKey = "", encoding = "hex" } = options;
+    return crypto.privateDecrypt(privateKey, Buffer.from(data, encoding)).toString();
+}
+
+function sign(data, options = {}) {
+    const { algorithm = "sha256", encoding = "hex" } = options;
+    const buffer = crypto.createSign(algorithm);
+    buffer.update(data);
+    buffer.end();
+    return buffer.sign(privateKey, encoding);
+}
+
+function verify(data, signature, options = {}) {
+    const { algorithm = "sha256", encoding = "hex" } = options;
+    const buffer = crypto.createVerify(algorithm);
+    buffer.update(data);
+    buffer.end();
+    return buffer.verify(privateKey, signature, encoding);
+}
+
+function base64Encode(data) {
+    return Buffer.from(data).toString("base64");
+}
+
+function base64Decode(data) {
+    return Buffer.from(data, "base64").toString();
+}
+
+function base64UrlEncode(data) {
+    return Buffer.from(data).toString("base64url");
+}
+
+function base64UrlDecode(data) {
+    return Buffer.from(data, "base64url").toString();
+}
+
+function hash(data, options = {}) {
+    const { algorithm = "sha256", encoding = "hex" } = options;
+    return crypto.createHash(algorithm).update(data).digest(encoding);
+}
+
+function hmac(data, options = {}) {
+    const { algorithm = "sha256", key = "", encoding = "hex" } = options;
+    return crypto.createHmac(algorithm, key).update(data).digest(encoding);
+}
+
+// // # Crypto
 // //
-// // Usage
+// // ### Install
+// // ```
+// // npm install @ndiing/crypto
+// // ```
 // //
+// // ### CDN
+// // ```
+// // https://cdn.jsdelivr.net/gh/ndiing/crypto/crypto.js
+// // ```
+// //
+// // ### Usage
+// //
+// // const crypto = require('@ndiing/crypto')
 // var data = "message";
 // var key = crypto.randomBytes(16).toString("hex");
 // var iv = crypto.randomBytes(8).toString("hex");
 // //
-// var encrypted = Crypto.encrypt(data, { key, iv });
+// var encrypted = encrypt(data, { key, iv });
 // console.log(encrypted)
-// console.log(Crypto.decrypt(encrypted, { key, iv }));
+// console.log(decrypt(encrypted, { key, iv }));
 // //
 // var { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
 //     modulusLength: 4096,
@@ -101,26 +111,41 @@ class Crypto {
 // var options={privateKey, publicKey}
 // var data='message'
 // //
-// var encrypted = Crypto.privateEncrypt(data, options);
+// var encrypted = privateEncrypt(data, options);
 // console.log(encrypted);
-// console.log(Crypto.publicDecrypt(encrypted, options));
-// var encrypted = Crypto.publicEncrypt(data, options);
+// console.log(publicDecrypt(encrypted, options));
+// var encrypted = publicEncrypt(data, options);
 // console.log(encrypted);
-// console.log(Crypto.privateDecrypt(encrypted, options));
-// var signature=Crypto.sign(encrypted, options)
+// console.log(privateDecrypt(encrypted, options));
+// var signature=sign(encrypted, options)
 // console.log(signature);
-// console.log(Crypto.verify(encrypted,signature, options));
+// console.log(verify(encrypted,signature, options));
 // //
-// var encoded=Crypto.base64Encode('message')
+// var encoded=base64Encode('message')
 // console.log(encoded)
-// console.log(Crypto.base64Decode(encoded))
+// console.log(base64Decode(encoded))
 // //
-// var encoded=Crypto.base64UrlEncode('message')
+// var encoded=base64UrlEncode('message')
 // console.log(encoded)
-// console.log(Crypto.base64UrlDecode(encoded))
+// console.log(base64UrlDecode(encoded))
 // //
-// console.log(Crypto.hash("password"));
-// console.log(Crypto.hmac("password", { key: "secret" }));
+// console.log(hash("password"));
+// console.log(hmac("password", { key: "secret" }));
 
-module.exports = Crypto;
+module.exports = {
+    encrypt,
+    decrypt,
+    privateEncrypt,
+    publicDecrypt,
+    publicEncrypt,
+    privateDecrypt,
+    sign,
+    verify,
+    base64Encode,
+    base64Decode,
+    base64UrlEncode,
+    base64UrlDecode,
+    hash,
+    hmac,
+};
 // window.Crypto = Crypto;
