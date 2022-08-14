23c6a231ef1690e509b6a47553a4debf71ac281c
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-14 10:45:30 +0700

    save

--- index.js
+++ index.js
@@ -1,6 +1,126 @@
 const crypto = require("crypto");
-const base32Encode = require("base32-encode");
-const base32Decode = require("base32-decode");
+
+function toDataView(data) {
+    if (data instanceof Int8Array || data instanceof Uint8Array || data instanceof Uint8ClampedArray) {
+        return new DataView(data.buffer, data.byteOffset, data.byteLength);
+    }
+
+    if (data instanceof ArrayBuffer) {
+        return new DataView(data);
+    }
+
+    throw new TypeError("Expected `data` to be an ArrayBuffer, Buffer, Int8Array, Uint8Array or Uint8ClampedArray");
+}
+
+const RFC4648 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+const RFC4648_HEX = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
+const CROCKFORD = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+
+function base32Encode(data, variant = "RFC4648", options) {
+    data = Buffer.from(data);//+
+    options = options || {};
+    let alphabet, defaultPadding;
+
+    switch (variant) {
+        case "RFC3548":
+        case "RFC4648":
+            alphabet = RFC4648;
+            defaultPadding = true;
+            break;
+        case "RFC4648-HEX":
+            alphabet = RFC4648_HEX;
+            defaultPadding = true;
+            break;
+        case "Crockford":
+            alphabet = CROCKFORD;
+            defaultPadding = false;
+            break;
+        default:
+            throw new Error("Unknown base32 variant: " + variant);
+    }
+
+    const padding = options.padding !== undefined ? options.padding : defaultPadding;
+    const view = toDataView(data);
+
+    let bits = 0;
+    let value = 0;
+    let output = "";
+
+    for (let i = 0; i < view.byteLength; i++) {
+        value = (value << 8) | view.getUint8(i);
+        bits += 8;
+
+        while (bits >= 5) {
+            output += alphabet[(value >>> (bits - 5)) & 31];
+            bits -= 5;
+        }
+    }
+
+    if (bits > 0) {
+        output += alphabet[(value << (5 - bits)) & 31];
+    }
+
+    if (padding) {
+        while (output.length % 8 !== 0) {
+            output += "=";
+        }
+    }
+
+    return output;
+}
+
+function readChar(alphabet, char) {
+    var idx = alphabet.indexOf(char);
+
+    if (idx === -1) {
+        throw new Error("Invalid character found: " + char);
+    }
+
+    return idx;
+}
+
+function base32Decode(input, variant = "RFC4648") {
+    var alphabet;
+
+    switch (variant) {
+        case "RFC3548":
+        case "RFC4648":
+            alphabet = RFC4648;
+            input = input.replace(/=+$/, "");
+            break;
+        case "RFC4648-HEX":
+            alphabet = RFC4648_HEX;
+            input = input.replace(/=+$/, "");
+            break;
+        case "Crockford":
+            alphabet = CROCKFORD;
+            input = input.toUpperCase().replace(/O/g, "0").replace(/[IL]/g, "1");
+            break;
+        default:
+            throw new Error("Unknown base32 variant: " + variant);
+    }
+
+    var length = input.length;
+
+    var bits = 0;
+    var value = 0;
+
+    var index = 0;
+    var output = new Uint8Array(((length * 5) / 8) | 0);
+
+    for (var i = 0; i < length; i++) {
+        value = (value << 5) | readChar(alphabet, input[i]);
+        bits += 5;
+
+        if (bits >= 8) {
+            output[index++] = (value >>> (bits - 8)) & 255;
+            bits -= 8;
+        }
+    }
+
+    return Buffer.from(output.buffer).toString();//+
+    // return output.buffer
+}
 
 /**
  * ### Install
@@ -203,22 +323,30 @@ class Crypto {
         const { algorithm = "sha256", key, encoding = "hex" } = options;
         return crypto.createHmac(algorithm, key).update(data).digest(encoding);
     }
+
+    /**
+     *
+     * @param {String} data
+     * @param {String} variant
+     * @returns {String}
+     */
+    static base32Encode(...args) {
+        return base32Encode(...args);
+    }
+
+    /**
+     *
+     * @param {String} input
+     * @param {String} variant
+     * @returns {String}
+     */
+    static base32Decode(...args) {
+        return base32Decode(...args);
+    }
 }
-
-/**
- * @param {String} input
- * @param {String} variant - `RFC3548`,`RFC4648`,`RFC4648-HEX`,`Crockford`
- * @method base32Encode
- */
-Crypto.base32Encode = (input, variant = "RFC4648") => base32Encode(input, variant);
-
-/**
- * @param {String} input
- * @param {String} variant - `RFC3548`,`RFC4648`,`RFC4648-HEX`,`Crockford`
- * @method base32Decode
- */
-Crypto.base32Decode = (input, variant = "RFC4648") => base32Decode(input, variant);
 
 module.exports = Crypto;
 
 // jsdoc2md crypto/index.js > crypto/README.md
+
+console.log(Crypto.base32Decode(Crypto.base32Encode("data")));
