fc4340dc62386c47aeb539f3064fddea1ef29073
Ridho Prasetya <ndiing.inc@gmail.com>
2022-08-16 11:05:12 +0700

    save

--- README.md
+++ README.md
@@ -1,6 +1,71 @@
 # crypto
-[Docs](https://ndiing.github.io/crypto/)
+crypto
 
+### Install
 ```
-npm install @ndiing/crypto
+npm install @ndiinginc/crypto
+```
+
+### Usage
+```js
+
+const Crypto = require('@ndiinginc/crypto')
+
+var options = {
+    key: crypto.randomBytes(16).toString("hex"),
+    iv: crypto.randomBytes(8).toString("hex"),
+};
+
+var encoded = Crypto.encrypt("data", options);
+console.log(encoded);
+var decoded = Crypto.decrypt(encoded, options);
+console.log(decoded);
+
+var options = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: "spki",
+        format: "pem",
+    },
+    privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem",
+    },
+});
+
+var encoded = Crypto.privateEncrypt("data", options);
+console.log(encoded);
+var decoded = Crypto.publicDecrypt(encoded, options);
+console.log(decoded);
+
+var encoded = Crypto.publicEncrypt("data", options);
+console.log(encoded);
+var decoded = Crypto.privateDecrypt(encoded, options);
+console.log(decoded);
+
+var sign = Crypto.sign("data", options);
+console.log(sign);
+var verify = Crypto.verify("data", sign, options);
+console.log(verify);
+
+var encoded = Crypto.base64Encode("data");
+console.log(encoded);
+var decoded = Crypto.base64Decode(encoded);
+console.log(decoded);
+
+var encoded = Crypto.base64UrlEncode("data");
+console.log(encoded);
+var decoded = Crypto.base64UrlDecode(encoded);
+console.log(decoded);
+
+var encoded = Crypto.base32Encode("data");
+console.log(encoded);
+var decoded = Crypto.base32Decode(encoded);
+console.log(decoded);
+
+var hash = Crypto.hash("data");
+console.log(hash);
+var hmac = Crypto.hmac("data");
+console.log(hmac);
+
 ```
--- index.js
+++ index.js
@@ -1,350 +1,144 @@
 const crypto = require("crypto");
 
-function toDataView(data) {
-    if (data instanceof Int8Array || data instanceof Uint8Array || data instanceof Uint8ClampedArray) {
-        return new DataView(data.buffer, data.byteOffset, data.byteLength);
-    }
-
-    if (data instanceof ArrayBuffer) {
-        return new DataView(data);
-    }
-
-    throw new TypeError("Expected `data` to be an ArrayBuffer, Buffer, Int8Array, Uint8Array or Uint8ClampedArray");
-}
-
-const RFC4648 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-const RFC4648_HEX = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
-const CROCKFORD = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
-
-function base32Encode(data, variant = "RFC4648", options) {
-    data = Buffer.from(data);//+
-    options = options || {};
-    let alphabet, defaultPadding;
-
-    switch (variant) {
-        case "RFC3548":
-        case "RFC4648":
-            alphabet = RFC4648;
-            defaultPadding = true;
-            break;
-        case "RFC4648-HEX":
-            alphabet = RFC4648_HEX;
-            defaultPadding = true;
-            break;
-        case "Crockford":
-            alphabet = CROCKFORD;
-            defaultPadding = false;
-            break;
-        default:
-            throw new Error("Unknown base32 variant: " + variant);
-    }
-
-    const padding = options.padding !== undefined ? options.padding : defaultPadding;
-    const view = toDataView(data);
-
-    let bits = 0;
-    let value = 0;
-    let output = "";
-
-    for (let i = 0; i < view.byteLength; i++) {
-        value = (value << 8) | view.getUint8(i);
-        bits += 8;
-
-        while (bits >= 5) {
-            output += alphabet[(value >>> (bits - 5)) & 31];
-            bits -= 5;
-        }
-    }
-
-    if (bits > 0) {
-        output += alphabet[(value << (5 - bits)) & 31];
-    }
-
-    if (padding) {
-        while (output.length % 8 !== 0) {
-            output += "=";
-        }
-    }
-
-    return output;
-}
-
-function readChar(alphabet, char) {
-    var idx = alphabet.indexOf(char);
-
-    if (idx === -1) {
-        throw new Error("Invalid character found: " + char);
-    }
-
-    return idx;
-}
-
-function base32Decode(input, variant = "RFC4648") {
-    var alphabet;
-
-    switch (variant) {
-        case "RFC3548":
-        case "RFC4648":
-            alphabet = RFC4648;
-            input = input.replace(/=+$/, "");
-            break;
-        case "RFC4648-HEX":
-            alphabet = RFC4648_HEX;
-            input = input.replace(/=+$/, "");
-            break;
-        case "Crockford":
-            alphabet = CROCKFORD;
-            input = input.toUpperCase().replace(/O/g, "0").replace(/[IL]/g, "1");
-            break;
-        default:
-            throw new Error("Unknown base32 variant: " + variant);
-    }
-
-    var length = input.length;
-
-    var bits = 0;
-    var value = 0;
-
-    var index = 0;
-    var output = new Uint8Array(((length * 5) / 8) | 0);
-
-    for (var i = 0; i < length; i++) {
-        value = (value << 5) | readChar(alphabet, input[i]);
-        bits += 5;
-
-        if (bits >= 8) {
-            output[index++] = (value >>> (bits - 8)) & 255;
-            bits -= 8;
-        }
-    }
-
-    return Buffer.from(output.buffer).toString();//+
-    // return output.buffer
-}
-
-/**
- * ### Install
- * ```
- * npm install @ndiing/crypto
- * ```
- * @see {@link ./test.js}
- */
 class Crypto {
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.algorithm=aes256
-     * @param {String} options.key
-     * @param {String} options.iv
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
+    // crypto
+
     static encrypt(data, options = {}) {
-        const { algorithm = "aes256", key, iv, encoding = "hex" } = options;
+        const { algorithm = "aes256", key = "", iv = "", encoding = "hex" } = options;
         const cipher = crypto.createCipheriv(algorithm, key, iv);
         return Buffer.concat([cipher.update(data), cipher.final()]).toString(encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.algorithm=aes256
-     * @param {String} options.key
-     * @param {String} options.iv
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static decrypt(data, options = {}) {
-        const { algorithm = "aes256", key, iv, encoding = "hex" } = options;
+        const { algorithm = "aes256", key = "", iv = "", encoding = "hex" } = options;
         const cipher = crypto.createDecipheriv(algorithm, key, iv);
         return Buffer.concat([cipher.update(data, encoding), cipher.final()]).toString();
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.privateKey
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static privateEncrypt(data, options = {}) {
-        const { privateKey, encoding = "hex" } = options;
-        const buffer = Buffer.from(data);
-        return crypto.privateEncrypt(privateKey, buffer).toString(encoding);
+        const { privateKey = "", encoding = "hex" } = options;
+        data = Buffer.from(data);
+        return crypto.privateEncrypt(privateKey, data).toString(encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.publicKey
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
-    static publicDecrypt(data, options = {}) {
-        const { publicKey, encoding = "hex" } = options;
-        const buffer = Buffer.from(data, encoding);
-        return crypto.publicDecrypt(publicKey, buffer).toString();
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.publicKey
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
-    static publicEncrypt(data, options = {}) {
-        const { publicKey, encoding = "hex" } = options;
-        const buffer = Buffer.from(data);
-        return crypto.publicEncrypt(publicKey, buffer).toString(encoding);
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.privateKey
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static privateDecrypt(data, options = {}) {
-        const { privateKey, encoding = "hex" } = options;
-        const buffer = Buffer.from(data, encoding);
-        return crypto.privateDecrypt(privateKey, buffer).toString();
+        const { privateKey = "", encoding = "hex" } = options;
+        data = Buffer.from(data, encoding);
+        return crypto.privateDecrypt(privateKey, data).toString();
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.algorithm=sha256
-     * @param {String/Object} options.privateKey
-     * @param {String} options.privateKey.key
-     * @param {String} options.privateKey.padding
-     * @param {String} options.privateKey.saltLength
-     * @param {String} options.privateKey.dsaEncoding
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
+    static publicEncrypt(data, options = {}) {
+        const { publicKey = "", encoding = "hex" } = options;
+        data = Buffer.from(data);
+        return crypto.publicEncrypt(publicKey, data).toString(encoding);
+    }
+
+    static publicDecrypt(data, options = {}) {
+        const { publicKey = "", encoding = "hex" } = options;
+        data = Buffer.from(data, encoding);
+        return crypto.publicDecrypt(publicKey, data).toString();
+    }
+
     static sign(data, options = {}) {
-        const { algorithm = "sha256", privateKey, encoding = "hex" } = options;
-        const buffer = crypto.createSign(algorithm);
-        buffer.write(data);
-        buffer.end();
-        return buffer.sign(privateKey, encoding);
+        const { algorithm = "sha256", privateKey = "", encoding = "hex" } = options;
+        const writable = crypto.createSign(algorithm);
+        writable.write(data);
+        writable.end();
+        return writable.sign(privateKey, encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {String} signature
-     * @param {Object} options
-     * @param {String} options.algorithm=sha256
-     * @param {String/Object} options.privateKey
-     * @param {String} options.privateKey.key
-     * @param {String} options.privateKey.padding
-     * @param {String} options.privateKey.saltLength
-     * @param {String} options.privateKey.dsaEncoding
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static verify(data, signature, options = {}) {
-        const { algorithm = "sha256", privateKey, encoding = "hex" } = options;
-        const buffer = crypto.createVerify(algorithm);
-        buffer.write(data);
-        buffer.end();
-        return buffer.verify(privateKey, signature, encoding);
+        const { algorithm = "sha256", privateKey = "", encoding = "hex" } = options;
+        const writable = crypto.createVerify(algorithm);
+        writable.write(data);
+        writable.end();
+        return writable.verify(privateKey, signature, encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @returns {String}
-     */
-    static base64Encode(data) {
-        return Buffer.from(data).toString("base64");
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @returns {String}
-     */
-    static base64Decode(data) {
-        return Buffer.from(data, "base64").toString();
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @returns {String}
-     */
-    static base64UrlEncode(data) {
-        return Buffer.from(data).toString("base64url");
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @returns {String}
-     */
-    static base64UrlDecode(data) {
-        return Buffer.from(data, "base64url").toString();
-    }
-
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.algorithm=sha256
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static hash(data, options = {}) {
         const { algorithm = "sha256", encoding = "hex" } = options;
         return crypto.createHash(algorithm).update(data).digest(encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {Object} options
-     * @param {String} options.algorithm=sha256
-     * @param {String} options.key
-     * @param {String} options.encoding=hex
-     * @returns {String}
-     */
     static hmac(data, options = {}) {
-        const { algorithm = "sha256", key, encoding = "hex" } = options;
+        const { algorithm = "sha256", key = "", encoding = "hex" } = options;
         return crypto.createHmac(algorithm, key).update(data).digest(encoding);
     }
 
-    /**
-     *
-     * @param {String} data
-     * @param {String} variant
-     * @returns {String}
-     */
-    static base32Encode(...args) {
-        return base32Encode(...args);
+    // base64
+    static base64Encode(data) {
+        return Buffer.from(data).toString("base64");
     }
 
-    /**
-     *
-     * @param {String} input
-     * @param {String} variant
-     * @returns {String}
-     */
-    static base32Decode(...args) {
-        return base32Decode(...args);
+    static base64Decode(data) {
+        return Buffer.from(data, "base64").toString();
+    }
+
+    // base64Url
+    static base64UrlEncode(data) {
+        return Buffer.from(data).toString("base64Url");
+    }
+
+    static base64UrlDecode(data) {
+        return Buffer.from(data, "base64Url").toString();
+    }
+
+    // base32
+    // https://www.rfc-editor.org/rfc/rfc4648
+    static base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+    static base32Encode(data) {
+        data = Buffer.from(data);
+
+        let alphabet = this.base32Alphabet;
+        let view = new DataView(data.buffer, data.byteOffset, data.byteLength);
+        let bits = 0;
+        let value = 0;
+        let output = "";
+
+        for (let i = 0; i < view.byteLength; i++) {
+            value = (value << 8) | view.getUint8(i);
+            bits += 8;
+
+            while (bits >= 5) {
+                output += alphabet[(value >>> (bits - 5)) & 31];
+                bits -= 5;
+            }
+        }
+
+        if (bits > 0) {
+            output += alphabet[(value << (5 - bits)) & 31];
+        }
+
+        while (output.length % 8 !== 0) {
+            output += "=";
+        }
+
+        return output;
+    }
+
+    static base32Decode(data) {
+        data = data.replace(/=+$/, "");
+
+        let alphabet = this.base32Alphabet;
+        let length = data.length;
+        let bits = 0;
+        let value = 0;
+        let index = 0;
+        let output = new Uint8Array(((length * 5) / 8) | 0);
+
+        for (let i = 0; i < length; i++) {
+            value = (value << 5) | alphabet.indexOf(data[i]);
+            bits += 5;
+
+            if (bits >= 8) {
+                output[index++] = (value >>> (bits - 8)) & 255;
+                bits -= 8;
+            }
+        }
+
+        return Buffer.from(output).toString();
     }
 }
 
 module.exports = Crypto;
-
-// jsdoc2md crypto/index.js > crypto/README.md
